e1ab9c4654a932a74c29a6826b85dbc01d002d03
Woshiajuana <zhigang.chen@owulia.com>
2018-12-06 15:26:47 +0800

    提交页面数据

--- service/src/app.js
+++ service/src/app.js
@@ -5,12 +5,32 @@ const app = new Koa();
 const server = http.createServer(app.callback());
 const io = require('socket.io')(server);
 
-io.on('connection', function (socket) {
-    socket.emit('connect success', { hello: 'world' });
-    socket.on('password', function (data) {
-        console.log(data);
+const controllerClient = {
+    '666': null,
+};    // 控制器
+const screenClient = {
+    '000': null,
+};        // 大屏幕
+
+io.on('connection', (socket) => {
+
+    // 触发链接成功
+    socket.emit('success', {});
+
+    // 验证口令
+    socket.on('password', (data) => {
+        console.log()
     });
+
+    // 断开链接
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+
 });
+
+
+
 
 
 server.listen(9090);
